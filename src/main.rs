a1bd08cdd8150e3f0bfc52d574402e26245b5f56
Antonio Diaz Flores <wisky.brandy@hotmail.com>
2024-12-30 21:46:11 -0800

    Replaced hard-coded string with TcpListener to use random port in tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run()?.await
+    let port = "8000";
+    let address = format!("127.0.0.1:{}", &port);
+    let listener = TcpListener::bind(&address).expect("Failed to bind to random port");
+    run(listener)?.await
 }
